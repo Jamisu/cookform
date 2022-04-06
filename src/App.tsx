45202b52ea861e844bb15d72f833cd69dea40325
jamisu <a.pocentek@gmail.com>
2022-04-06 16:55:41 +0200

    Initiall commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useForm } from 'react-hook-form';
 
-function App() {
+import "./styles.css";
+
+export default function App() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = handleSubmit((data) => {
+    selectedDishOptions = data.type;
+    console.log(selectedDishOptions)
+  });
+
+  let selectedDishOptions = ''
+
+  function returnOptions() {
+    if(selectedDishOptions === 'Pizza') {
+      return (<input type="text" placeholder="Name2" {...register("Name2", {})} />)
+    } else {
+      return (<input type="text" placeholder="Name3" {...register("Name3", {})} />)
+    }
+  }
+  //console.log('errors', errors);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={onSubmit}>
+      <input type="text" placeholder="Name" {...register("Name", {required: 'error message'})} />
+      <input type="number" placeholder="preparation_time" {...register("preparation_time", {required: 'error message'})} />
+      <select {...register("type", {required: 'error message'})}>
+        <option value="Pizza" selected>Pizza</option>
+        <option value="Soup"> Soup</option>
+        <option value="Sandwich"> Sandwich</option>
+      </select>
+      {returnOptions()}
+      <input type="submit" />
+    </form>
   );
 }
 
-export default App;
---
+++ src/components/input.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+//import './tile.css';
+//import buttonHolder from './buttonHolder'
+
+function inputfield() {
+    const tile = <div className='tile'>
+                    input
+                </div>
+                
+    return tile
+}
+
+export {
+    inputfield
+}
