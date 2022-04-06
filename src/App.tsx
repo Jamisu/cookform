b107cfafa0ab2a6f6d96a9afe9893895fa972365
jamisu <a.pocentek@gmail.com>
2022-04-06 18:22:56 +0200

    logic improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 import "./styles.css";
 
 export default function App() {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const { register, handleSubmit, unregister, formState: { errors } } = useForm();
   const onSubmit = handleSubmit((data) => {
-    selectedDishOptions = data.type;
-    console.log(selectedDishOptions)
+    console.log('SUBMIT', data)
   });
 
-  let selectedDishOptions = ''
+  const [selectedDishOptions, updateDish] = useState<string>('');
+  
 
   function returnOptions() {
+    console.log('selectedDishOptions', selectedDishOptions);
+
     if(selectedDishOptions === 'Pizza') {
-      return (<input type="text" placeholder="Name2" {...register("Name2", {})} />)
+      return (<input type="text" id="111" key="111" className="one" placeholder="Name2" {...register("Dish.Name1", {})} />)
     } else {
-      return (<input type="text" placeholder="Name3" {...register("Name3", {})} />)
+      return (<input type="text" id="222" key="222" className="two" placeholder="Name3" {...register("Dish.Name2", {})} />)
     }
   }
-  //console.log('errors', errors);
+
+  function onDishTypeChange(targetValue:any) {
+    unregister("Dish");
+    updateDish(targetValue);
+  }
   
   return (
     <form onSubmit={onSubmit}>
       <input type="text" placeholder="Name" {...register("Name", {required: 'error message'})} />
-      <input type="number" placeholder="preparation_time" {...register("preparation_time", {required: 'error message'})} />
-      <select {...register("type", {required: 'error message'})}>
-        <option value="Pizza" selected>Pizza</option>
-        <option value="Soup"> Soup</option>
-        <option value="Sandwich"> Sandwich</option>
+      <input type="number" placeholder="preparation_time" {...register("preparation_time",
+        { min: {
+          value: 3,
+          message: 'error message'
+        }
+      })} />
+      <select {...register("type", {required: 'error message', onChange: (e) => onDishTypeChange(e.target.value)})}>
+        <option value="">Select...</option>
+        <option value="Pizza">Pizza</option>
+        <option value="Soup">Soup</option>
+        <option value="Sandwich">Sandwich</option>
       </select>
       {returnOptions()}
       <input type="submit" />
     </form>
   );
 }
-
