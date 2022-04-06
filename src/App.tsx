88a5b6410378e7da1f0d60c30893642a4bb1eb7f
jamisu <a.pocentek@gmail.com>
2022-04-06 21:20:18 +0200

    minor changes, validation handling

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ import "./styles.css";
 
 export default function App() {
   const { register, handleSubmit, unregister, formState: { errors } } = useForm();
-  const [dish, updateDish] = useState<string>('');
+  const [dish, updateDish] = useState<string>('Pizza');
   const errorMsg = {
     req: 'this field is required',
-    num: 'should be grater than one minute'
+    num: 'should be grater than zero'
   }
   const onSubmit = handleSubmit((data) => {
     console.log('SUBMIT', data)
@@ -22,29 +22,40 @@ export default function App() {
     }
   }
 
-  function onDishTypeChange(targetValue:any) {
+  function onDishTypeChange(targetValue:string) {
     unregister("Dish");
     updateDish(targetValue);
   }
+
+  console.log("errors", errors);
   
-  return (
+  return (    
     <form onSubmit={onSubmit}>
+      <h1>
+        Cook Form
+      </h1>
       <input type="text" placeholder="Name" {...register("Name", {required: errorMsg.req})} />
-      <input type="number" placeholder="preparation_time" {...register("preparation_time",
+      {errors.Name && <div className="error">{errors.Name.message}</div>}
+
+      <input type="number" defaultValue="0" placeholder="preparation_time" {...register("preparation_time",
         { min: {
           value: 1,
           message: errorMsg.num
         },
         required: errorMsg.req
       })} />
-      <select {...register("type", {required: errorMsg.req, onChange: (e) => onDishTypeChange(e.target.value)})}>
-        <option value="">Select...</option>
+      {errors.preparation_time && <div className="error">{errors.preparation_time.message}</div>}
+
+      <select placeholder="Enter your name" {...register("type", {required: errorMsg.req, onChange: (e) => onDishTypeChange(e.target.value)})}>
         <option value="Pizza">Pizza</option>
         <option value="Soup">Soup</option>
         <option value="Sandwich">Sandwich</option>
       </select>
+
       {returnOptions()}
+
       <input type="submit" />
+
     </form>
   );
 }
